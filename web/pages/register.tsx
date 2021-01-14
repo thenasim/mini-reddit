7e3f6191e0be66431156a677eaa82d7957f0a220
Nasim Uddin <ficklenasim@outlook.com>
2021-01-14 12:11:45 +0600

    basic urql setup and register

--- web/pages/register.tsx
+++ web/pages/register.tsx
@@ -2,24 +2,52 @@ import React from "react";
 import { Form, Formik } from "formik";
 import Wrapper from "../components/Wrapper";
 import InputField from "../components/InputField";
+import { useMutation } from "urql";
 
 interface Props {}
 
+const REGISTER_MUT = `
+mutation Register($username: String!, $password: String!, $name: String!) {
+  register (options: {username: $username, password: $password, name: $name}) {
+    errors {
+      field
+      message
+    }
+    user {
+      id
+      name
+      username
+    }
+  }
+}`;
+
 const Register: React.FC<Props> = () => {
+  const [, register] = useMutation(REGISTER_MUT);
+
   return (
     <Wrapper>
       <Formik
-        initialValues={{ username: "", password: "" }}
-        onSubmit={(values) => {
-          console.log(values);
+        initialValues={{ username: "", password: "", name: "" }}
+        onSubmit={async (values) => {
+          const response = await register(values);
         }}
       >
         {({ isSubmitting }) => (
           <Form className="mt-6">
             <InputField
+              label="Full Name"
+              id="name"
+              type="text"
+              name="name"
+              placeholder="full name"
+              autoComplete="none"
+              className="block w-full p-3 mt-2 text-gray-700 bg-gray-200 appearance-none focus:outline-none focus:bg-gray-300 focus:shadow-inner"
+              required
+            />
+            <InputField
               label="Username"
               id="username"
-              type="username"
+              type="text"
               name="username"
               placeholder="your username"
               autoComplete="none"
@@ -42,7 +70,7 @@ const Register: React.FC<Props> = () => {
             >
               {isSubmitting ? "....." : "Sign up"}
             </button>
-            <p className="flex justify-between inline-block mt-4 text-xs text-gray-500 cursor-pointer hover:text-black">
+            <p className="flex justify-between mt-4 text-xs text-gray-500 cursor-pointer hover:text-black">
               Already registered?
             </p>
           </Form>
